1a2593578348e48b97b2ff111b53c099a9494196
ywliao <yiliu4234@163.com>
2017-06-21 13:10:04 +0800

    Update Ampl_to_Gene_region.R

--- Ampl_to_Gene_region.R
+++ Ampl_to_Gene_region.R
@@ -1,7 +1,9 @@
 library(data.table)
 
-amplicon <- fread("/home/ywliao/project/Gengyan/amplicon_primer.txt") # Gene column is neccesary
+amplicon <- fread("/home/ywliao/project/Gengyan/amplicon_primer.txt") 
 GeneAnno <- fread("/home/ywliao/utilities/UCSC/hg19annotation",skip = 1,col.names = c("Chr","Start","End","KgID","Gene","Description"))
+GeneAnno <- GeneAnno[,.(Chr=Chr,Start=min(Start),End=max(End)),by="Gene"]
 setkey(GeneAnno,Chr,Start,End,Gene)
-Gene.bed <- GeneAnno[amplicon$Gene,.(Chr,Start,End,Gene),on="Gene",mult="last"]
+Gene.bed <- unique(GeneAnno[amplicon$Gene,.(Chr,Start,End,Gene),on="Gene"])
+setkey(Gene.bed,Chr,Start,End,Gene)
 fwrite(Gene.bed,file="/home/ywliao/project/Gengyan/GeneRegion.bed",sep="\t",col.names = F)
